685e2ab9f9ad5c9425d38ddd8ccb97ae752e951b
MarcA <marcarbiolrams@gmail.com>
2025-05-09 14:46:33 +0200

    Creacio dels constructors

--- Llibre.java
+++ Llibre.java
@@ -1,7 +1,15 @@
 public class Llibre {
-    
-    // ATRIBUT
+
+    // ATRIBUTS DE LA CLASSE
     private String titol;
     private String autor;
     private boolean prestat;
+
+    // CONSTRUCORS DE LA CLASSE
+    // Constructor de la classe que rep tots els atributs de la classe.
+    public Llibre(String titol, String autor) {
+        this.titol = titol;
+        this.autor = autor;
+        this.prestat = false;
+    }
 }
